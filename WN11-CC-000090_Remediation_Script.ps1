4ebc516daced4137a4868034e24a6f9665e67cbb
Brad Han <haneungue@gmail.com>
2025-10-30 17:53:51 -0400

    Update WN11-CC-000090_Remediation_Script.ps1

--- WN11-CC-000090_Remediation_Script.ps1
+++ WN11-CC-000090_Remediation_Script.ps1
@@ -8,8 +8,8 @@
     Author          : Brad Han
     LinkedIn        : https://www.linkedin.com/in/brad-han/
     GitHub          : https://github.com/behan101
-    Date Created    : 10/29/2025
-    Last Modified   : 10/29/2025
+    Date Created    : 10/30/2025
+    Last Modified   : 10/30/2025
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
